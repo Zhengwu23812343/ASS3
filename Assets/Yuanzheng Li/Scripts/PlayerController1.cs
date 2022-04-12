8e9f05122ca2d1d80215d77abc78ebdef1e3f8e7
noahbrilliance <1299260067@qq.com>
2022-04-12 17:40:56 +0800

    add win

--- Assets/Yuanzheng Li/Scripts/PlayerController1.cs
+++ Assets/Yuanzheng Li/Scripts/PlayerController1.cs
@@ -8,17 +8,23 @@ public class PlayerController1 : MonoBehaviour
     private Rigidbody rb;
     private int count;
     public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        winTextObject.SetActive(false);
     }
 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if(count >= 100)
+        {
+            winTextObject.SetActive(true);
+        }
 
     }
 
